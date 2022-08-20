66bfd4440500482cf36bdb6bfa89b748c5268e24
YuriiPohorilets <pohorilets.y@gmail.com>
2022-08-20 19:04:10 +0300

    tast #2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+var throttle = require('lodash.throttle');
+
+const setCurrentTimeUpdate = function (data) {
+  const currentTime = data.seconds;
+
+  localStorage.setItem('videoplayer-current-time', currentTime);
+};
+
+player.on('timeupdate', throttle(setCurrentTimeUpdate, 1_000));
+
+const getCurrentTimeUpdate = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(getCurrentTimeUpdate)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
